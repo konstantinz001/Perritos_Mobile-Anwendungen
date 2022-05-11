93776479e0d04ec828f4a2aa16f962b6f437008e
ma981son <ma981son@htwg-konstanz.de>
2022-05-11 15:11:27 +0200

    add(login_screen): Added Textfields and Icon Button

--- lib/screens/registration_and_login/registration_and_login_view.dart
+++ lib/screens/registration_and_login/registration_and_login_view.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/assets/styles/perritos-colors.dart';
 import 'package:flutter_application/assets/styles/perritos-fonts.dart';
+import 'package:flutter_application/assets/styles/perritos-icons/perritos_icons.dart';
 import 'package:flutter_application/assets/ui-components/buttons/perritos-button.dart';
+import 'package:flutter_application/assets/ui-components/buttons/perritos-icon-button.dart';
+import 'package:flutter_application/assets/ui-components/text-input/perritos_txt_input.dart';
 import 'package:flutter_application/common/providers.dart';
 import 'package:flutter_application/screens/registration_and_login/registration_and_login_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -66,24 +70,59 @@ class RegistrationAndLoginView extends ConsumerWidget {
                       ),
                       const SizedBox(height: 20),
                       PerritosButton(
-                        onPressed: () => {print('Sign Up')}, 
+                        onPressed: () => {print('Sign Up')},
                         label: 'Sign Up'
                       ),
                     ]
-                ) : Column(
+                ) : Container(
+                color: PerritosColor.perritosSnow.color,
+                child:
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 10,
+                    top: 0,
+                    right: 10,
+                    bottom: 0,
+                  ),
+                  child: Column(
                     children: [
-                      Text(
-                        'Welcome back!',
-                        style: perritosDoubleParagon,
+                      const SizedBox(height: 60,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(width: 10),
+                          Expanded(child:
+                          PerritosIconButton(
+                              onPressed: () => controller.switchCurrentRegistrationAndLoginScreen(RegistrationAndLogin.kickoff),
+                              label: "",
+                              icon: PerritosIcons.Icon_Arrow_Left)
+                          ),
+                          Text(
+                            'Welcome back!',
+                            style: perritosDoubleParagon,
+                            textAlign: TextAlign.center,
+                          ),
+                          Spacer()
+                        ],
                       ),
-                      const SizedBox(height: 20),
+                      const SizedBox(height: 36),
+                      PerritosTxtInput(
+                        onSubmit: (value) => {print(value)},
+                        hintTxt: "E-Mail Adresse",),
+                      PerritosTxtInput(
+                        onSubmit: (value) => {print(value)},
+                        hintTxt: "Passwort",),
+                      Spacer(),
                       PerritosButton(
-                        onPressed: () => {print('Login')}, 
-                        label: 'Login'
+                          onPressed: () => {print('Login')},
+                          label: 'Login'
                       ),
-                    ]
-                ) 
+                      SizedBox(height: 60)
+                    ],
+                  ),
+                )
       ),
+      )
     );
   }
 }
