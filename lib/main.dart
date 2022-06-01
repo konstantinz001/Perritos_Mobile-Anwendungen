ff8ec5f0d5ca873f6d6ecb08b6c05e252c56803e
Konstantin Zabaznov <konsi.zabaznov@web.de>
2022-06-01 18:46:27 +0200

    UserProfil

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/screens/registration_and_login/registration_and_login_view.dart';
+import 'package:flutter_application/screens/user_selection_and_administration/user_selection_and_administration_view.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
@@ -18,29 +19,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Navigator(
-        initialRoute: '/RegistrationAndLogin',
-        onGenerateRoute: (RouteSettings routeParams) {
-          if (routeParams.name == '/RegistrationAndLogin') {
-            return MaterialPageRoute(builder: (context) => const RegistrationAndLoginView());
-          } else if (routeParams.name == '/UserSelectionAndAdministration') {
-            return MaterialPageRoute(builder: (context) => const Center(child: Text('UserSelectionAndAdministration Screen')));
-          } else if (routeParams.name == '/DogSelectionAndAdministration') {
-            return MaterialPageRoute(builder: (context) => const Center(child: Text('DogSelectionAndAdministration Screen')));
-          } else if (routeParams.name == '/Home') {
-            return MaterialPageRoute(builder: (context) => const Center(child: Text('Home Screen')));
-          } else if (routeParams.name == '/Calendar') {
-            return MaterialPageRoute(builder: (context) => const Center(child: Text('Calendar Screen')));
-          } else if (routeParams.name == '/DogProfileInfo') {
-            return MaterialPageRoute(builder: (context) => const Center(child: Text('DogProfileInfo Screen')));
-          } 
+        home: Navigator(
+      initialRoute: '/RegistrationAndLogin',
+      onGenerateRoute: (RouteSettings routeParams) {
+        if (routeParams.name == '/RegistrationAndLogin') {
           return MaterialPageRoute(
-              builder: (context) => const Center(child: Text('Unkown Screen')));
-        },
-        onPopPage: (route, result) {
-          return route.didPop(result);
-        },
-      )
-    );
+              builder: (context) => const RegistrationAndLoginView());
+        } else if (routeParams.name == '/UserSelectionAndAdministration') {
+          return MaterialPageRoute(
+              builder: (context) => const UserSelectionAndAdministrationView());
+        } else if (routeParams.name == '/DogSelectionAndAdministration') {
+          return MaterialPageRoute(
+              builder: (context) => const Center(
+                  child: Text('DogSelectionAndAdministration Screen')));
+        } else if (routeParams.name == '/Home') {
+          return MaterialPageRoute(
+              builder: (context) => const Center(child: Text('Home Screen')));
+        } else if (routeParams.name == '/Calendar') {
+          return MaterialPageRoute(
+              builder: (context) =>
+                  const Center(child: Text('Calendar Screen')));
+        } else if (routeParams.name == '/DogProfileInfo') {
+          return MaterialPageRoute(
+              builder: (context) =>
+                  const Center(child: Text('DogProfileInfo Screen')));
+        }
+        return MaterialPageRoute(
+            builder: (context) => const Center(child: Text('Unkown Screen')));
+      },
+      onPopPage: (route, result) {
+        return route.didPop(result);
+      },
+    ));
   }
 }
