054a10a923461e45b014bc6c04b102ea38323418
Konstantin Zabaznov <konsi.zabaznov@web.de>
2022-06-01 19:00:33 +0200

    UserProfil

--- lib/screens/registration_and_login/registration_and_login_view.dart
+++ lib/screens/registration_and_login/registration_and_login_view.dart
@@ -20,84 +20,87 @@ class RegistrationAndLoginView extends ConsumerWidget {
         ref.watch(providers.registrationAndLoginControllerProvider);
 
     return Scaffold(
-
-      body: Center(
-        child:
-            model.currentRegistrationAndLoginScreen ==
-                    RegistrationAndLogin.kickoff
-                ? 
-                Container(
-                  color: PerritosColor.perritosSnow.color,
-                  child: 
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 10,
-                        top: 0,
-                        right: 10,
-                        bottom: 0,
-                      ),
-                      child:
-                        Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const Image(
-                                image: AssetImage('lib/assets/images/Perritos_Logo_1_Coloured.png'),
-                                height: 227,
-                              ),
-                              const SizedBox(height: 60),
-                              PerritosButton(onPressed: () => {controller.switchCurrentRegistrationAndLoginScreen(
-                                          RegistrationAndLogin.login)}, label: 'Login'),
-                              const SizedBox(height: 20),
-                              PerritosButton(
-                                onPressed: () => {controller.switchCurrentRegistrationAndLoginScreen(
-                                          RegistrationAndLogin.registration)}, 
-                                label: 'Sign Up'
-                              ),
-                              const SizedBox(height: 60),
-                            ],
-                        )
-                    )
-                )
-                : model.currentRegistrationAndLoginScreen ==
-                    RegistrationAndLogin.registration 
-                ? Container(
-                color: PerritosColor.perritosSnow.color,
-                child:
-                Padding(
+        body: Center(
+      child: model.currentRegistrationAndLoginScreen ==
+              RegistrationAndLogin.kickoff
+          ? Container(
+              color: PerritosColor.perritosSnow.color,
+              child: Padding(
                   padding: const EdgeInsets.only(
                     left: 10,
                     top: 0,
                     right: 10,
                     bottom: 0,
                   ),
-                  child: Column (
-                      children: [
-                        const SizedBox(height: 60,),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Image(
+                        image: AssetImage(
+                            'lib/assets/images/Perritos_Logo_1_Coloured.png'),
+                        height: 227,
+                      ),
+                      const SizedBox(height: 60),
+                      PerritosButton(
+                          onPressed: () => {
+                                controller
+                                    .switchCurrentRegistrationAndLoginScreen(
+                                        RegistrationAndLogin.login)
+                              },
+                          label: 'Login'),
+                      const SizedBox(height: 20),
+                      PerritosButton(
+                          onPressed: () => {
+                                controller
+                                    .switchCurrentRegistrationAndLoginScreen(
+                                        RegistrationAndLogin.registration)
+                              },
+                          label: 'Sign Up'),
+                      const SizedBox(height: 60),
+                    ],
+                  )))
+          : model.currentRegistrationAndLoginScreen ==
+                  RegistrationAndLogin.registration
+              ? Container(
+                  color: PerritosColor.perritosSnow.color,
+                  child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        top: 0,
+                        right: 10,
+                        bottom: 0,
+                      ),
+                      child: Column(children: [
+                        const SizedBox(
+                          height: 60,
+                        ),
                         Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               const SizedBox(width: 10),
-                              Expanded(child:
-                              PerritosIconButton(
-                                  onPressed: () => controller.switchCurrentRegistrationAndLoginScreen(RegistrationAndLogin.kickoff),
-                                  iconSize: 40,
-                                  icon: PerritosIcons.Icon_Arrow_Left) 
-                          ),
+                              Expanded(
+                                  child: PerritosIconButton(
+                                      onPressed: () => controller
+                                          .switchCurrentRegistrationAndLoginScreen(
+                                              RegistrationAndLogin.kickoff),
+                                      iconSize: 40,
+                                      icon: PerritosIcons.Icon_Arrow_Left)),
                               Text(
                                 'Welcome!',
                                 style: perritosDoubleParagon,
                               ),
                               const Spacer()
-                            ]
-                        ),
+                            ]),
                         const SizedBox(height: 36),
-                        PerritosTxtInput( 
-                          onSubmit: (value) => {print(value)},
-                          hintTxt: "Username",),
                         PerritosTxtInput(
                           onSubmit: (value) => {print(value)},
-                          hintTxt: "E-Mail Adresse",),
+                          hintTxt: "Username",
+                        ),
+                        PerritosTxtInput(
+                          onSubmit: (value) => {print(value)},
+                          hintTxt: "E-Mail Adresse",
+                        ),
                         PerritosTxtInput(
                           onSubmit: (value) => {print(value)},
                           hintTxt: "Passwort",
@@ -110,65 +113,65 @@ class RegistrationAndLoginView extends ConsumerWidget {
                         ),
                         const Spacer(),
                         PerritosButton(
-                        onPressed: () => {print('Sign Up')},
-                        label: 'Sign Up'
-                        ),
+                            onPressed: () => {print('Sign Up')},
+                            label: 'Sign Up'),
                         const SizedBox(height: 60)
-                    ]
-                  )
-                )
-                ) : Container(
-                color: PerritosColor.perritosSnow.color,
-                child:
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10,
-                    top: 0,
-                    right: 10,
-                    bottom: 0,
-                  ),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 60,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(width: 10),
-                          Expanded(child:
-                          PerritosIconButton(
-                              onPressed: () => controller.switchCurrentRegistrationAndLoginScreen(RegistrationAndLogin.kickoff),
-                              iconSize: 40,
-                              icon: PerritosIcons.Icon_Arrow_Left) 
-                          ),
-                          Text(
-                            'Welcome back!',
-                            style: perritosDoubleParagon,
-                            textAlign: TextAlign.center,
-                          ),
-                          const Spacer()
-                        ],
-                      ),
-                      const SizedBox(height: 36),
-                      PerritosTxtInput( 
-                        onSubmit: (value) => {print(value)},
-                        hintTxt: "E-Mail Adresse",),
-                      PerritosTxtInput(
-                        onSubmit: (value) => {print(value)},
-                        hintTxt: "Passwort",
-                        password: true,
+                      ])))
+              : Container(
+                  color: PerritosColor.perritosSnow.color,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      top: 0,
+                      right: 10,
+                      bottom: 0,
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 60,
                         ),
-                      const Spacer(),
-                      PerritosButton(
-                          onPressed: () => {Navigator.pushNamed(context, '/Home')},
-                          label: 'Login'
-                      ),
-                      const SizedBox(height: 60)
-                    ],
-                  ),
-                )
-      ),
-      )
-    );
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(width: 10),
+                            Expanded(
+                                child: PerritosIconButton(
+                                    onPressed: () => controller
+                                        .switchCurrentRegistrationAndLoginScreen(
+                                            RegistrationAndLogin.kickoff),
+                                    iconSize: 40,
+                                    icon: PerritosIcons.Icon_Arrow_Left)),
+                            Text(
+                              'Welcome back!',
+                              style: perritosDoubleParagon,
+                              textAlign: TextAlign.center,
+                            ),
+                            const Spacer()
+                          ],
+                        ),
+                        const SizedBox(height: 36),
+                        PerritosTxtInput(
+                          onSubmit: (value) => {print(value)},
+                          hintTxt: "E-Mail Adresse",
+                        ),
+                        PerritosTxtInput(
+                          onSubmit: (value) => {print(value)},
+                          hintTxt: "Passwort",
+                          password: true,
+                        ),
+                        const Spacer(),
+                        PerritosButton(
+                            onPressed: () => {
+                                  Navigator.pushNamed(context,
+                                      '/UserSelectionAndAdministration')
+                                },
+                            label: 'Login'),
+                        const SizedBox(height: 60)
+                      ],
+                    ),
+                  )),
+    ));
   }
 }
 
