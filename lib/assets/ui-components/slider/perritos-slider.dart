004f98b16a23d3bc49acb9a0355343d44f18199e
ma981son <ma981son@htwg-konstanz.de>
2022-06-01 13:47:04 +0200

    add(slider): new slider component

---
+++ lib/assets/ui-components/slider/perritos-slider.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application/assets/styles/perritos-colors.dart';
+import 'package:flutter_application/assets/styles/perritos-icons/PerritosIcons_icons.dart';
+
+class PerritosSlider extends StatefulWidget {
+  PerritosSlider({Key? key, this.value = 0}) : super(key: key);
+
+  double value;
+
+  @override
+  _PerritosSliderState createState() => _PerritosSliderState();
+}
+
+class _PerritosSliderState extends State<PerritosSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                SizedBox(width: 10),
+                Icon(
+                  PerritosIcons.Icon_Smiley_Happy,
+                  size: 21,
+                  color: PerritosColor.perritosGoldFusion.color,
+                ),
+                Spacer(),
+                Icon(
+                  PerritosIcons.Icon_Smiley_Sad,
+                  size: 21,
+                  color: PerritosColor.perritosBurntSienna.color,
+                ),
+                SizedBox(width: 10)
+              ],
+            )),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+                child: SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    trackHeight: 4,
+                    thumbShape:
+                    const RoundSliderThumbShape(enabledThumbRadius: 9.0),
+                    overlayShape:
+                    const RoundSliderOverlayShape(overlayRadius: 9.0),
+                  ),
+                  child: Slider(
+                      min: 0,
+                      max: 10,
+                      activeColor: PerritosColor.perritosSandyBrown.color,
+                      inactiveColor: PerritosColor.perritosCharcoal.color.withOpacity(0.5),
+                      value: widget.value,
+                      onChanged: (newValue) {
+                        setState(() {
+                          widget.value = newValue;
+                        });
+                      }),
+                ))
+          ],
+        )
+      ],
+    ));
+  }
+}
