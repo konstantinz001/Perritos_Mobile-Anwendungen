21983040945776fd2467987c70bf6bbbff63fdae
WalterVoetschCortes <walter.vxd@googlemail.com>
2022-06-06 14:06:41 +0200

    fix(main): initialRoute

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         localizationsDelegates: const [GlobalMaterialLocalizations.delegate],
         supportedLocales: const [Locale('en'), Locale('de')],
         home: Navigator(
-          initialRoute: '/Calendar',
+          initialRoute: '/RegistrationAndLogin',
           onGenerateRoute: (RouteSettings routeParams) {
             if (routeParams.name == '/RegistrationAndLogin') {
               return MaterialPageRoute(
