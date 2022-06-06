45f2903eabcab114b04cbfcae927b61cac9f1da4
WalterVoetschCortes <walter.vxd@googlemail.com>
2022-06-06 13:20:56 +0200

    add(calendar): simple content to calendar view

--- lib/screens/calendar/calendar_view.dart
+++ lib/screens/calendar/calendar_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/assets/styles/perritos-colors.dart';
+import 'package:flutter_application/assets/styles/perritos-icons/PerritosIcons_icons.dart';
+import 'package:flutter_application/assets/ui-components/buttons/perritos-icon-button.dart';
+import 'package:flutter_application/assets/ui-components/navigation/perritos-navigation.dart';
 import 'package:flutter_application/common/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import 'calendar_model.dart';
 
@@ -11,16 +16,63 @@ class CalendarView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final CalendarController controller =
         ref.read(providers.calendarControllerProvider.notifier);
-    final CalendarModel model =
-        ref.watch(providers.calendarControllerProvider);
+    final CalendarModel model = ref.watch(providers.calendarControllerProvider);
 
-    return const Scaffold(body: Center(child: Text('Calendar Screen')));
-    
+    return Scaffold(
+      body: Container(
+      color: PerritosColor.perritosSnow.color,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 10,
+          top: 10,
+          right: 10,
+          bottom: 10,
+        ),
+        child: Column(children: [
+          Row(
+            children: [
+              PerritosIconButton(
+                  onPressed: () =>
+                      {print("go to dog-selection-and-administration view")},
+                  icon: PerritosIcons.Icon_Dog,
+                  iconSize: 26,
+              ),
+              const Spacer(),
+              PerritosIconButton(
+                  onPressed: () =>
+                    {print("go to user-selection-and-administration view")},
+                  icon: PerritosIcons.Icon_User,
+                  iconSize: 26,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20,),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(children: 
+                [
+                  TableCalendar(
+                    locale: 'de',
+                    focusedDay: DateTime.now(), 
+                    firstDay: DateTime(DateTime.now().year - 10, DateTime.now().month, DateTime.now().day),
+                    lastDay: DateTime(DateTime.now().year + 10, DateTime.now().month, DateTime.now().day),
+                  )     
+                ]
+              )
+            )
+          ),
+          PerritosNavigationBar(
+            activeView: activeView.calendar, 
+            navigateToHome: () => {print("go to hme view")}, 
+            navigateToCalendar: () => {print("go to calendar view")}, 
+            navigateToProfile: () => {print("go to profile view")}
+          )
+        ],)
+      ),
+    ));
   }
 }
 
-abstract class CalendarController
-        extends StateNotifier<CalendarModel> {
-  CalendarController(CalendarModel state)
-      : super(state);
+abstract class CalendarController extends StateNotifier<CalendarModel> {
+  CalendarController(CalendarModel state) : super(state);
 }
