aa7d79df319029ee52174aec2cc4f2e6e458c5c1
WalterVoetschCortes <walter.vxd@googlemail.com>
2022-06-07 18:30:07 +0200

    rework(calendar_controller): after meeting

--- lib/screens/calendar/calendar_controller.dart
+++ lib/screens/calendar/calendar_controller.dart
@@ -3,6 +3,66 @@ import 'package:flutter_application/screens/calendar/calendar_model.dart';
 import 'package:intl/intl.dart';
 import 'calendar_view.dart';
 
+//(.....,Startdatum, Enddatum)
+var actionDates = {
+  DateTime.parse(
+      "${DateFormat("yyyy-MM-dd").format(DateTime.now())} 00:00:00.000Z"): [
+    ActionDateModel(
+      "Impfung",
+      "Tierarzt Praxis Lümmel",
+      DateTime(2022, 7, 8, 17, 30),
+      DateTime(2022, 7, 8, 18, 30),
+      ["user1", "user2"],
+      ["dog1", "dog2"],
+    ),
+    ActionDateModel(
+      "Arzt",
+      "Tierarzt Praxis Lümmel",
+      DateTime(2022, 7, 12, 17, 30),
+      DateTime(2022, 7, 14, 18, 30),
+      ["user1", "user2"],
+      ["dog1", "dog2"],
+    ),
+    ActionDateModel(
+      "Gassi mit Kumpel",
+      "im Park",
+      DateTime(2022, 7, 20, 17, 30),
+      DateTime(2022, 7, 20, 18, 30),
+      ["user1", "user2"],
+      ["dog1", "dog2"],
+    ),
+  ]
+};
+var actionDates1 = {
+  DateTime.parse(
+      "${DateFormat("yyyy-MM-dd").format(DateTime(2022, 7, 8, 17, 30))} 00:00:00.000Z"): [
+    ActionDateModel(
+      "Impfung",
+      "Tierarzt Praxis Lümmel",
+      DateTime(2022, 7, 8, 17, 30),
+      DateTime(2022, 7, 8, 18, 30),
+      ["user1", "user2"],
+      ["dog1", "dog2"],
+    ),
+    ActionDateModel(
+      "Arzt",
+      "Tierarzt Praxis Lümmel",
+      DateTime(2022, 7, 12, 17, 30),
+      DateTime(2022, 7, 14, 18, 30),
+      ["user1", "user2"],
+      ["dog1", "dog2"],
+    ),
+    ActionDateModel(
+      "Gassi mit Kumpel",
+      "im Park",
+      DateTime(2022, 7, 20, 17, 30),
+      DateTime(2022, 7, 20, 18, 30),
+      ["user1", "user2"],
+      ["dog1", "dog2"],
+    ),
+  ]
+};
+
 class CalendarImplmentation extends CalendarController {
   CalendarImplmentation({
     CalendarModel? model,
@@ -12,26 +72,8 @@ class CalendarImplmentation extends CalendarController {
                     "${DateFormat("yyyy-MM-dd").format(DateTime.now())} 00:00:00.000Z"),
                 focusedDay: DateTime.parse(
                     "${DateFormat("yyyy-MM-dd").format(DateTime.now())} 00:00:00.000Z"),
-                selectedEvents: {
-                  DateTime.parse("2022-06-09 00:00:00.000Z"): [
-                    ActionDateModel(
-                      "Arzt Besuch",
-                      "Tierarzt Praxis Lümmel",
-                      DateTime(2022, 7, 8, 17, 30),
-                      DateTime(2022, 7, 8, 18, 30),
-                      ["user1", "user2"],
-                      ["dog1", "dog2"],
-                    ),
-                    ActionDateModel(
-                      "Impfung",
-                      "Tierarzt Praxis Lümmel",
-                      DateTime(2022, 7, 8, 17, 30),
-                      DateTime(2022, 7, 8, 18, 30),
-                      ["user1", "user2"],
-                      ["dog1", "dog2"],
-                    ),
-                  ]
-                }));
+                selectedEvents: new Map.from(actionDates)
+                  ..addAll(actionDates1)));
 
   @override
   List<ActionDateModel> getEventsfromDay(DateTime date) {
