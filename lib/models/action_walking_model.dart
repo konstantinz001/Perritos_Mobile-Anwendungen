a533a2cf01e4b96cfac0121bc8ab9ee4ed984581
WalterVoetschCortes <walter.vxd@googlemail.com>
2022-06-07 17:34:09 +0200

    add(model): model for other action types

---
+++ lib/models/action_walking_model.dart
@@ -0,0 +1,13 @@
+class ActionDateModel {
+  final DateTime begin;
+  final DateTime end;
+  final List<String> users;
+  final List<String> dogs;
+
+  ActionDateModel(
+    this.begin,
+    this.end,
+    this.users,
+    this.dogs
+  );
+}
