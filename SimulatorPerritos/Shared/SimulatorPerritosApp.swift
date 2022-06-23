e37707e480697ede66c803a2fdf9b20135cc7afe
WalterVoetschCortes <walter.vxd@googlemail.com>
2022-06-23 12:53:34 +0200

    fix(perritos): ios support

---
+++ SimulatorPerritos/Shared/SimulatorPerritosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimulatorPerritosApp.swift
+//  Shared
+//
+//  Created by Walter Vötsch Cortés on 23.06.22.
+//
+
+import SwiftUI
+
+@main
+struct SimulatorPerritosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
